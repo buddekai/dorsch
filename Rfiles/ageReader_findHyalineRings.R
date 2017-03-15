48911088d033a749d485149ca7ec87d33fe7ee72
buddekai <kaibudde@gmail.com>
2017-03-15 14:45:48 +0100

    Hyaline rings can now be found.

--- Rfiles/ageReader_findHyalineRings.R
+++ Rfiles/ageReader_findHyalineRings.R
@@ -6,6 +6,11 @@ findHyalineRings <- function(image.grey, image.information,
   
   # Parameter Sektion ####
   parameter.for.hyaline <- 1.1
+  points.to.jump <- 5
+  #wie oft soll versucht werden die Lücken zuschliessen:
+  repeate.fill.up <- 2
+  # Wie lang soll eine zusammenhaengende Markierung mindestens sein?
+  min.hyaline.length <- 48
   
   # Parameter bis hier
   
@@ -36,6 +41,7 @@ findHyalineRings <- function(image.grey, image.information,
   names(df.connecting.line) <- c("x", "y")
   df.connecting.line$hyaline <- 0
   
+  
   for(i in 1:number.of.points.along.line){
     
     current.point <- image.grey[connecting.line[i,2],
@@ -46,11 +52,61 @@ findHyalineRings <- function(image.grey, image.information,
     }
   }
   
+  
+  # Versuche Luecken aufzufuellen
+  
+  while(repeate.fill.up > 0){
+    points.to.jump.copy <- points.to.jump
+    
+    while(points.to.jump > 0){
+      pattern <- c(1, rep(0, points.to.jump), 1)
+      pattern <- paste(pattern, collapse = "")
+      
+      replacement <- c(1, rep(1, points.to.jump), 1)
+      replacement <- paste(replacement, collapse = "")
+      
+      hyaline.vector <- paste(df.connecting.line$hyaline, collapse = "")
+      
+      hyaline.vector <- gsub(pattern, replacement, hyaline.vector)
+      
+      df.connecting.line$hyaline <- 
+        as.numeric(unlist(strsplit(hyaline.vector, split = "")))
+      
+      points.to.jump <- points.to.jump - 1
+    }
+    
+    points.to.jump <- points.to.jump.copy
+    repeate.fill.up <- repeate.fill.up - 1
+  }
+  
+  marker <- NULL
+  # Loesche alle Markierungen die kuerzer sind als der Parameter
+  for(i in 1:length(df.connecting.line$hyaline)){
+    if(df.connecting.line$hyaline[i] == 1){
+      marker <- c(marker, i)
+    }else{
+      marker <- NULL
+    }
+    
+    if(length(marker) >= min.hyaline.length ){
+      df.connecting.line$hyaline[marker] <- 3
+    }
+  }
+  
+  df.connecting.line$hyaline[df.connecting.line$hyaline != 3] <- 0
+  df.connecting.line$hyaline[df.connecting.line$hyaline == 3] <- 1
+  
+  
+  
+  
   hyline.points <- connecting.line[which(df.connecting.line$hyaline==1),]
   
   image.information[cbind(hyline.points[,2], hyline.points[,1], 3)] <- -1
   image.information[cbind(hyline.points[,2], hyline.points[,1], 2)] <- -1
   image.information[cbind(hyline.points[,2], hyline.points[,1], 1)] <- 1
+  
+  
+  
   
   return(image.information)
   
