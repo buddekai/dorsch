4b464480a5e0d5021b81b390e71d037045b78743
buddekai <kaibudde@gmail.com>
2017-03-30 10:13:56 +0200

    We can now read the scale.

--- Rfiles/testAufloesung.R
+++ Rfiles/testAufloesung.R
@@ -1,1 +1,17 @@
 # Einlesen der czi-Datei fuer den genauen Wert der Aufloesung
+
+input.file <- "input/otoliths_czi/DEM1416_001.czi"
+metadata <- paste(readLines(input.file, n = 461, skipNul = TRUE), collapse = " ")
+metadata <- gsub(".*<Metadata>", "<Metadata>", metadata)
+metadata <- gsub(" +", "", metadata)
+scale <- gsub(".*(<Items>.+</Items>).*", "\\1", metadata)
+scale.x <- as.numeric(gsub(".*<DistanceId=\"X\"><Value>(.{1,30})</Value>.*", "\\1", scale))
+scale.y <- as.numeric(gsub(".*<DistanceId=\"Y\"><Value>(.{1,30})</Value>.*", "\\1", scale))
+
+if(scale.x == scale.y){
+  # Jetzt kann die Skalierung abgespeichert werden.
+  print("alles gut")
+}else{
+  print("Scales in x- and y-directions are not the same.")
+}
+
